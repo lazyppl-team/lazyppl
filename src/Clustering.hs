2b40cc8c16d00400a4759345f8b54838995e5d19
Hugo Paquet <huguet@clpc324.cs.ox.ac.uk>
2021-05-13 17:51:34 +0100

    add missing log in lwis

--- src/Clustering.hs
+++ src/Clustering.hs
@@ -14,9 +14,6 @@ import Data.List
 import Data.Maybe
 import Control.Monad
 import Control.Monad.State.Lazy (State, state , put, get, runState)
-import Data.Map (empty,lookup,insert,size,keys)
-import Data.IORef
-import System.IO.Unsafe
 
 import Data.Default.Class
 import Control.Lens
@@ -24,7 +21,7 @@ import Data.Monoid
 
 import Debug.Trace
 
-{-- Stick breaking breaks the unit interval into an 
+{-- Stick breaking breaks the unit interval into an
     infinite number of parts (lazily) --}
 stickBreaking :: Double -> Double -> Prob [Double]
 stickBreaking alpha lower =
@@ -40,34 +37,34 @@ iid p = do {r <- p ; rs <- iid p ; return $ r : rs}
 dp :: Double -> Prob a -> Prob (Prob a)
 dp alpha p = do xs <- iid p
                 vs <- stickBreaking alpha 0
-                return $ do r <- uniform 
+                return $ do r <- uniform
                             return $ xs !! (fromJust $ findIndex (> r) (scanl1 (+) vs))
 
-{-- Return a distribution over distributions 
+{-- Return a distribution over distributions
     saying what the chance of getting the
     first, second, third bit of stick is etc --}
-crp :: Double -> Prob (Prob Int) 
+crp :: Double -> Prob (Prob Int)
 crp alpha = do vs <- stickBreaking alpha 0
-               return $ do r <- uniform 
+               return $ do r <- uniform
                            return $ fromJust $ findIndex (> r) (scanl1 (+) vs)
 
-{-- A generic stick-breaking-based clustering program 
-    Takes pparam :: Prob b, a distribution on parameters 
+{-- A generic stick-breaking-based clustering program
+    Takes pparam :: Prob b, a distribution on parameters
           like :: b -> a -> Double, parameterized likelihood function \
     Tags each point with a colour describing the cluster it is in --}
 cluster :: [a] -> (Prob b) -> (b -> a -> Double) -> Meas [(a,Double)]
 cluster xs pparam like =
         do pcluster <- sample $ crp 0.3
            param    <- sample $ memoize $ \i -> pparam
-           color    <- sample $ memoize $ \i -> uniform 
+           color    <- sample $ memoize $ \i -> uniform
            mapM (\x -> do {i <- sample pcluster ; score $ like (param i) x ; return (x,(color i))}) xs
 
 -- Example 2d data set
 dataset = [(7.7936387,7.469271),(5.3105156,7.891521),(5.4320135,5.135559),(7.3844196,7.478719),(6.7382938,7.476735),(0.6663453,4.460257),(3.2001898,2.653919),(2.1231227,3.758051),(3.3734472,2.420528),(0.4699408,1.835277)]
 
 {-- Example of using the clustering program.
-    Here the parameters are the x and y coordinates 
-    and standard deviation of the cluster --} 
+    Here the parameters are the x and y coordinates
+    and standard deviation of the cluster --}
 example :: Meas [((Double,Double),Double)]
 example = cluster dataset (do {x <- normal 5 4 ; y <- normal 5 4 ; prec <- gamma 2 4 ; return (x,y,1/(sqrt prec)) })
                   (\(x,y,s) (x',y') -> normalPdf x s x' * normalPdf y s y')
@@ -81,29 +78,8 @@ test =
     plot_coords "clustering.svg" xyc
 
 
-{-- Stochastic memoization.
-    We use unsafePerformIO to maintain 
-    a table of calls that have already been made. 
-    If a is finite, we could just sample all values of a in advance 
-    and avoid unsafePerformIO. 
-    If it is countably infinite, there probably also implementation tricks. 
---} 
-memoize :: Ord a => (a -> Prob b) -> Prob (a -> b)
-memoize f = Prob $ do g <- get
-                      let ( (Tree _ gs), g2) = splitTree g
-                      put g2
-                      return $ unsafePerformIO $ do
-                                ref <- newIORef Data.Map.empty
-                                return $ \x -> unsafePerformIO $ do
-                                          m <- liftM (Data.Map.lookup x) (readIORef ref)
-                                          case m of
-                                              Just y -> return y
-                                              Nothing -> do let (Prob m) = f x 
-                                                            n <- readIORef ref
-                                                            let (y,_) = runState m (gs !! (1 + size n))
-                                                            modifyIORef' ref (Data.Map.insert x y)
-                                                            return y
- 
+
+
 
 
 {-- GRAPHING ROUTINES --}
@@ -123,11 +99,9 @@ plot_coords filename xycs =
      renderableToFile def filename graphic;
      putStrLn (" Done!")
      return ()
-    
+
 
 
 
 main :: IO ()
-main = do { test } 
-
-
+main = do { test }
--- src/LazyPPL.hs
+++ src/LazyPPL.hs
@@ -5,13 +5,16 @@ module LazyPPL where
 import Control.Monad.Trans.Writer
 import Control.Monad.Trans.Class
 import Data.Monoid
-import System.Random 
+import System.Random
 import Control.Monad
 import Control.Monad.Extra
 import Control.Monad.State.Lazy (State, state , put, get, runState)
 import Numeric.Log
+import Data.Map (empty,lookup,insert,size,keys)
+import Data.IORef
+import System.IO.Unsafe
 
-{-- 
+{--
     This file defines
     * Two monads: Prob (for probabilities) and Meas (for unnormalized probabilities)
     * Two inference methods: lwis and mh
@@ -20,14 +23,14 @@ import Numeric.Log
 -- A "Tree" is a lazy, infinitely wide and infinitely deep tree, labelled by Doubles
 -- Our source of randomness will be a Tree, populated by uniform [0,1] choices for each label.
 -- Often people would just use a list or stream instead of a tree.
--- But a tree allows us to be lazy about how far we are going all the time. 
+-- But a tree allows us to be lazy about how far we are going all the time.
 data Tree = Tree Double [Tree]
 
 -- A probability distribution over a is a state transformer over trees
 -- ie a function Tree -> (a , Tree)
 -- The idea is that it uses up bits of the tree as it runs
 newtype Prob a = Prob (State Tree a)
-  
+
 -- Two key things to do with trees:
 -- Split tree splits a tree in two (bijectively)
 -- Get the label at the head of the tree and discard the rest
@@ -71,18 +74,18 @@ scorelog r = Meas $ tell $ Product $ Exp $ (if r==0 then exp(-300) else r)
 sample :: Prob a -> Meas a
 sample p = Meas $ lift p
 
-{-- Preliminaries for the simulation methods. Generate a tree with uniform random labels 
-    This uses SPLIT to split a random seed --} 
+{-- Preliminaries for the simulation methods. Generate a tree with uniform random labels
+    This uses SPLIT to split a random seed --}
 randomTree :: RandomGen g => g -> Tree
 randomTree g = let (a,g') = random g in Tree a (randomTrees g')
 randomTrees :: RandomGen g => g -> [Tree]
 randomTrees g = let (g1,g2) = split g in (randomTree g1) : (randomTrees g2)
 
-{-- Run prob runs a probability deterministically, given a source of randomness --} 
+{-- Run prob runs a probability deterministically, given a source of randomness --}
 runProb :: Prob a -> Tree -> a
 runProb (Prob a) rs = fst $ runState a rs
 
-{-- weightedsamples runs a probability measure and gets out a stream of (result,weight) pairs --} 
+{-- weightedsamples runs a probability measure and gets out a stream of (result,weight) pairs --}
 weightedsamples :: forall a. Meas a -> IO [(a,Log Double)]
 weightedsamples (Meas m) =
                     do let helper :: Prob [(a,(Product (Log Double)))]
@@ -93,47 +96,50 @@ weightedsamples (Meas m) =
                        g <- getStdGen
                        let rs = randomTree g
                        let xws = runProb helper rs
-                       return $ map (\(x,w) -> (x,getProduct w)) xws 
+                       return $ map (\(x,w) -> (x,getProduct w)) xws
 
-{-- Likelihood weighted importance sampling first draws n weighted samples, 
-    and then samples a stream of results from that regarded as an empirical distribution --} 
+{-- Likelihood weighted importance sampling first draws n weighted samples,
+    and then samples a stream of results from that regarded as an empirical distribution --}
 lwis :: Int -> Meas a -> IO [a]
 lwis n m =        do xws <- weightedsamples m
                      let xws' = take n $ accumulate xws 0
                      let max = snd $ last xws'
                      g <- getStdGen
                      let rs = (randoms g :: [Double])
-                     return $ map (\r -> fst $ head $ filter (\(x,w)-> w>=(Exp $ r) * max) xws') rs
+                     return $ map (\r -> fst $ head $ filter (\(x,w)-> w>=(Exp $ log r) * max) xws') rs
 accumulate ((x,w):xws) a = (x,w+a):(accumulate xws (w+a))
 accumulate [] a = []
 
 
 
-{-- MH: Produce a stream of samples, using Metropolis Hastings 
-    We use (mutatetree p) to propose different distributions. 
-    If p = 1/dimension then this is a bit like single-site lightweight MH. 
-    If p = 1 then this is like multi-site lightweight MH --} 
+{-- MH: Produce a stream of samples, using Metropolis Hastings
+    We use (mutatetree p) to propose different distributions.
+    If p = 1/dimension then this is a bit like single-site lightweight MH.
+    If p = 1 thimport Data.Map (empty,lookup,insert,size,keys)
+import Data.IORef
+import System.IO.Unsafe
+en this is like multi-site lightweight MH --}
 mh :: forall a. Double -> Meas a -> IO [(a,Product (Log Double))]
 mh p (Meas m) = do
      -- Top level: produce a stream of samples.
      -- Split the random number generator in two
      -- One part is used as the first seed for the simulation,
-     -- and one part is used for the randomness in the MH algorithm. 
+     -- and one part is used for the randomness in the MH algorithm.
      g <- getStdGen
      let (g1,g2) = split g
      let t = randomTree g1
      let (x, w) = runProb (runWriterT m) t
      -- Now run step over and over to get a stream of (tree,result,weight)s.
      let (samples,_) = runState (iterateM step (t,x,w)) g2
-     -- The stream of seeds is used to produce a stream of result/weight pairs. 
+     -- The stream of seeds is used to produce a stream of result/weight pairs.
      return $ map (\(t,x,w) -> (x,w)) $ samples
      -- NB There are three kinds of randomness in the step function.
      -- * The start tree t, which is the source of randomness for simulating the
-     --   program m to start with. This is sort-of the point in the "state space". 
+     --   program m to start with. This is sort-of the point in the "state space".
      -- * The randomness needed to propose a new tree (g1)
      -- * The randomness needed to decide whether to accept or reject that (g2)
      -- The tree t is an argument and result,
-     -- but we use a state monad (get/put) to deal with the other randomness (g,g1,g2) 
+     -- but we use a state monad (get/put) to deal with the other randomness (g,g1,g2)
      where step :: RandomGen g => (Tree,a,Product (Log Double)) -> State g (Tree,a,Product (Log Double))
            step (t,x,w) = do
              -- Randomly change some sites
@@ -143,15 +149,15 @@ mh p (Meas m) = do
              -- Rerun the model with the new tree, to get a new
              -- weight w'.
              let (x', w') = runProb (runWriterT m) t'
-             -- MH acceptance ratio. This is the probability of either 
-             -- returning the new seed or the old one. 
+             -- MH acceptance ratio. This is the probability of either
+             -- returning the new seed or the old one.
              let ratio = (getProduct w') / (getProduct w )
              let (r,g2') = random g2
              put g2'
              if r < (min 1 (exp $ ln $ ratio)) then return (t',x',w') else return (t,x,w)
 
 
--- Replace the labels of a tree randomly, with probability p 
+-- Replace the labels of a tree randomly, with probability p
 mutateTree :: forall g. RandomGen g => Double -> g -> Tree -> Tree
 mutateTree p g (Tree a ts) = let (a',g') = (random g :: (Double,g)) in let (a'',g'') = random g' in
                                   Tree (if a'<p then a'' else a) (mutateTrees p g'' ts)
@@ -161,9 +167,31 @@ mutateTrees p g (t:ts) = let (g1,g2) = split g in (mutateTree p g1 t) : (mutateT
 
 
 {-- Useful function which thins out a list. --}
-every :: Int -> [a] -> [a] 
+every :: Int -> [a] -> [a]
 every n xs = case drop (n-1) xs of
               (y:ys) -> y : every n ys
               [] -> []
 
 
+{-- Stochastic memoization.
+    We use unsafePerformIO to maintain
+    a table of calls that have already been made.
+    If a is finite, we could just sample all values of a in advance
+    and avoid unsafePerformIO.
+    If it is countably infinite, there probably also implementation tricks.
+--}
+memoize :: Ord a => (a -> Prob b) -> Prob (a -> b)
+memoize f = Prob $ do g <- get
+                      let ( (Tree _ gs), g2) = splitTree g
+                      put g2
+                      return $ unsafePerformIO $ do
+                                ref <- newIORef Data.Map.empty
+                                return $ \x -> unsafePerformIO $ do
+                                          m <- liftM (Data.Map.lookup x) (readIORef ref)
+                                          case m of
+                                              Just y -> return y
+                                              Nothing -> do let (Prob m) = f x
+                                                            n <- readIORef ref
+                                                            let (y,_) = runState m (gs !! (1 + size n))
+                                                            modifyIORef' ref (Data.Map.insert x y)
+                                                            return y
