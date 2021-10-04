861306522465eba10f1fadbd200cc18a147f9b72
samstaton <sam.staton@cs.ox.ac.uk>
2021-10-04 12:32:12 +0100

    Dirichlet Process

---
+++ src/Distr/DirichletP.hs
@@ -0,0 +1,49 @@
+module Distr.DirichletP (Restaurant, Table, newCustomer,newRestaurant, dp) where
+import LazyPPL
+import Distr
+
+import Data.List
+import Data.Maybe
+
+{--
+Abstract types for the CRP. 
+e.g. 
+S. Staton, H. Yang, N. L.. Ackerman, C. Freer, D. Roy. 
+Exchangeable random process and data abstraction. 
+Workshop on probabilistic programming semantics (PPS 2017).
+--}
+
+data Restaurant = R [Double]
+data Table = T Int deriving (Eq,Ord,Show)
+
+newCustomer :: Restaurant -> Prob Table 
+newCustomer (R restaurant) =
+                do r <- uniform
+                   return $ T $ fromJust $ findIndex (> r) (scanl1 (+) restaurant)
+
+
+newRestaurant :: Double -> Prob Restaurant   
+newRestaurant alpha = do sticks <- stickBreaking alpha 0
+                         return $ R sticks 
+
+
+
+{-- Stick breaking breaks the unit interval into an
+    infinite number of parts (lazily) --}
+stickBreaking :: Double -> Double -> Prob [Double]
+stickBreaking alpha lower =
+  do r <- beta 1 alpha
+     let v = r * (1 - lower)
+     vs <- stickBreaking alpha v
+     return (v : vs)
+
+{-- We can then define the Dirichlet Process --}
+iid :: Prob a -> Prob [a]
+iid p = do {r <- p ; rs <- iid p ; return $ r : rs}
+
+dp :: Double -> Prob a -> Prob (Prob a)
+dp alpha p = do xs <- iid p
+                vs <- stickBreaking alpha 0
+                return $ do r <- uniform
+                            return $ xs !! (fromJust $ findIndex (> r) (scanl1 (+) vs))
+
