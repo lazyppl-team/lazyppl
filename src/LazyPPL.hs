c39fe7a74549599a93c80bf4f63619db8855c1ae
samstaton <sam.staton@cs.ox.ac.uk>
2022-03-05 18:51:19 +0000

    attempt at mixing mh with (mh 1)

--- src/LazyPPL.hs
+++ src/LazyPPL.hs
@@ -177,6 +177,49 @@ mutateTrees p g (t:ts) = let (g1,g2) = split g in mutateTree p g1 t : mutateTree
 
 
 
+{-- Irreducible form of mh. Takes p like mh, but also q, which is the chance of proposing an all-sites change. --}
+
+mhirreducible :: forall a. Double -> Double -> Meas a -> IO [(a,Product (Log Double))]
+mhirreducible p q (Meas m) = do
+    -- | Top level: produce a stream of samples.
+    -- | Split the random number generator in two
+    -- | One part is used as the first seed for the simulation,
+    -- | and one part is used for the randomness in the MH algorithm.
+    newStdGen
+    g <- getStdGen
+    let (g1,g2) = split g
+    let t = randomTree g1
+    let (x, w) = runProb (runWriterT m) t
+    -- | Now run step over and over to get a stream of (tree,result,weight)s.
+    let (samples,_) = runState (iterateM step (t,x,w)) g2
+    -- | The stream of seeds is used to produce a stream of result/weight pairs.
+    return $ map (\(t,x,w) -> (x,w)) samples
+    {- | NB There are three kinds of randomness in the step function.
+    1. The start tree 't', which is the source of randomness for simulating the
+    program m to start with. This is sort-of the point in the "state space".
+    2. The randomness needed to propose a new tree ('g1')
+    3. The randomness needed to decide whether to accept or reject that ('g2')
+    The tree t is an argument and result,
+    but we use a state monad ('get'/'put') to deal with the other randomness '(g,g1,g2)' -}
+    where step :: RandomGen g => (Tree,a,Product (Log Double)) -> State g (Tree,a,Product (Log Double))
+          step (t,x,w) = do
+            -- | Randomly change some sites
+            g <- get
+            let (g1,g2) = split g
+            -- | Decide whether to resample all sites (r<q) or just some of them
+            let (r,g1') = random g1
+            let t' = if r<q then randomTree g1' else mutateTree p g1' t
+            -- | Rerun the model with the new tree, to get a new
+            -- | weight w'.
+            let (x', w') = runProb (runWriterT m) t'
+            -- | MH acceptance ratio. This is the probability of either
+            -- | returning the new seed or the old one.
+            let ratio = getProduct w' / getProduct w
+            let (r,g2') = random g2
+            put g2'
+            if r < min 1 (exp $ ln ratio) then return (t',x',w') else return (t,x,w)
+
+
 
 
 
