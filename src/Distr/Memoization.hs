9c00e9fedb78ed7dd3d8f7912505fdd7178ef122
Sam Staton <sam.staton@cs.ox.ac.uk>
2024-08-12 19:46:22 +0100

    haddock

--- src/Distr/Memoization.hs
+++ src/Distr/Memoization.hs
@@ -2,6 +2,26 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE DefaultSignatures #-}
 
+{- | Stochastic memoization for the `LazyPPL` library.
+Stochastic memoization is a useful primitive for programming non-parametric models. 
+It has type @(a -> Prob b) -> Prob (a -> b)@, and can be thought of as simultaneously sampling results for all possible arguments, in a lazy way. 
+
+When @a@ is enumerable, this amounts to converting a stream of probabilities to a random stream, which we can do by sampling each probability once. 
+
+This module provides:
+
+* A general type-class `MonadMemo` for monads @m@ that support memoization at certain argument types @a@. 
+
+* A default trie-based implementation when @a@ is enumerable, and a curry-based implementation when @a@ is a pair type. 
+
+* A general implementation `generalmemoize` for probability, using memo-tables.
+
+* A memoized recursion combinator, `memrec`. 
+
+
+For illustrations, see the [graph example](https://lazyppl-team.github.io/GraphDemo.html), [clustering](https://lazyppl-team.github.io/ClusteringDemo.html), [additive clustering](https://lazyppl-team.github.io/AdditiveClusteringDemo.html), or the [infinite relational model](https://lazyppl-team.github.io/IrmDemo.html). 
+-}
+
 module Distr.Memoization (MonadMemo, memoize, generalmemoize, memrec) where
 
 import Control.Monad
@@ -11,9 +31,10 @@ import Data.IORef
 import Data.List
 import Data.Map (empty, insert, keys, lookup, size)
 import Debug.Trace
-import LazyPPL
+import LazyPPL 
 import System.IO.Unsafe
 
+{-| Type class for memoizable argument types @a@ under a monad @m@ -}
 class (Monad m) => MonadMemo m a where
   memoize :: (a -> m b) -> m (a -> b)
   default memoize :: (Enum a) => (a -> m b) -> m (a -> b)
@@ -33,18 +54,20 @@ look :: BinTree a -> Int -> a
 look (Branch x l r) 0 = x
 look (Branch _ l r) n = if even n then look r (n `div` 2) else look l (n `div` 2)
 
+{-| Implementation for enumerable types using tries -}
 instance (Monad m) => MonadMemo m Int
 
+{-| Implementation for pair types using currying -}
 instance (Monad m, MonadMemo m a, MonadMemo m b) => MonadMemo m (a, b) where
   memoize f = fmap uncurry $ memoize $ \x -> memoize $ \y -> f (x, y)
 
-{-- Stochastic memoization.
+{-| A general memoization method when @m@ is a probability monad.
+
     We use unsafePerformIO to maintain
     a table of calls that have already been made.
-    If a is finite, we could just sample all values of a in advance
-    and avoid unsafePerformIO.
-    If it is countably infinite, there probably also are implementation tricks.
---}
+    If @a@ is finite, we could just sample all values of @a@ in advance
+    and avoid unsafePerformIO. If @a@ is enumerable, we can use the trie method. 
+-}
 generalmemoize :: Ord a => (a -> Prob b) -> Prob (a -> b)
 generalmemoize f = Prob $ \(Tree _ gs) ->
   unsafePerformIO $ do
